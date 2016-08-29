a3414584f753f913d8a535a5eb7194307ecea78d
samba <opensamba@gmail.com>
2016-08-29 13:28:03 +0200

    adding conf to move back/forward one word

---
+++ custom/key-shortcut.zsh
@@ -0,0 +1,7 @@
+# some custom key needed in screen
+  bindkey "^[[1~" 	beginning-of-line      # [Home] - Go to beginning of line
+  bindkey "^[[4~"  	end-of-line            # [End] - Go to end of line
+
+  bindkey "^[[D" backward-word
+  bindkey "^[[C" forward-word
+
